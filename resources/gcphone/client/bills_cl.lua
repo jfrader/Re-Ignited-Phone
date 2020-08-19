bceadf623b8f1b5fd1e7ee61f7ab1f7f741d0dee
Yagiz ALP <yagizwasmeow@gmail.com>
2020-08-20 00:43:06 +0300

    Update bills_cl.lua

--- resources/gcphone/client/bills_cl.lua
+++ resources/gcphone/client/bills_cl.lua
@@ -1,12 +1,12 @@
-RegisterNetEvent("kidz:bills_getBilling")
-AddEventHandler("kidz:bills_getBilling", function(billingg)
+RegisterNetEvent("gcPhone:bills_getBilling")
+AddEventHandler("gcPhone:bills_getBilling", function(billingg)
   SendNUIMessage({event = 'bills_billingg', billingg = billingg})
 end)
 
 RegisterNUICallback('bills_getBilling', function(data, cb)
-  TriggerServerEvent('kidz:bills_getBilling', data.label, data.amount, data.sender)
+  TriggerServerEvent('gcPhone:bills_getBilling', data.label, data.amount, data.sender)
 end)
 
 RegisterNUICallback('bills_payBill', function(data)
-  TriggerServerEvent('kidz:billspayBill', data.id, data.sender, data.amount, data.target)
+  TriggerServerEvent('gcPhone:billspayBill', data.id, data.sender, data.amount, data.target)
 end)
